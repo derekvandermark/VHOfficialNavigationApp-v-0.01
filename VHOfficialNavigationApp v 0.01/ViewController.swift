8ef9eca3093e074860a0aebf9d4ad0170a2e3b7c
derekvandermark <derek.vandermark@gmail.com>
2018-07-13 17:31:20 -0400

    Initial Commit

---
+++ VHOfficialNavigationApp v 0.01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VHOfficialNavigationApp v 0.01
+//
+//  Created by Derek Vandermark on 7/13/18.
+//  Copyright © 2018 Derek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
